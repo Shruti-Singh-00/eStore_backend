5fe2e4059438825a3499850ca4f58fdb4dc9b0a1
Shruti-Singh-00 <ishrutisingh1010@gmail.com>
2024-07-16 05:00:46 +0530

    Final commit

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const cors = require('cors');
 const app = express();
 const PORT = 5001;
 const bodyparser = require('body-parser');
+const orders = require('./routes/orders');
 
 app.use(cors());
 app.use(bodyparser.json()); // bodyparser - Resolves the data ereceived in the body of the request before it passes it to the relevant route.
 app.use('/productCategories', productCategories);
 app.use('/products', products);
 app.use('/users', users);
+app.use('/orders', orders);
 
 const server = app.listen(PORT, () => {
     console.log('App is running on the port - 5001');
