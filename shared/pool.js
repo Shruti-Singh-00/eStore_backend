ae852611a1f8196b5098cb5aa8bbf4171c6365e8
Shruti-Singh-00 <107168579+Shruti-Singh-00@users.noreply.github.com>
2024-07-16 12:56:32 +0530

    Update pool.js

--- shared/pool.js
+++ shared/pool.js
@@ -1,11 +1,20 @@
 const mysql = require('mysql2');
 
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '#admin@12345',
+//     database: 'estore',
+//     port: 3306,
+//     multipleStatements: true    //Allow to have multiple queries..
+// });
+
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '#admin@12345',
+    host: 'mysql-f082e3d-estore-project.i.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_diGPVbP2N7M1DGf7mr0',
     database: 'estore',
-    port: 3306,
+    port: 15404,
     multipleStatements: true    //Allow to have multiple queries..
 });
 
